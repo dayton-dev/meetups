8756c700c97dd74113101447e80c3bbec873e32b
John Weis <weis.john@gmail.com>
2015-02-11 16:06:34 -0500

    fix jshint

--- generate-table.js
+++ generate-table.js
@@ -2,18 +2,18 @@
 
 // generate a markdown table from `meetups.json`
 
-var meetups = require('./meetups')
+var meetups = require('./meetups');
 
 // get the lables from the keys
 
 // if the label is not in the first entry, we won't find it
 var labels = Object.keys(meetups[0]);
 // this is kind of gross, in that i'm just deleting the rsvp entry
-labels.splice(1, 1)
+labels.splice(1, 1);
 
 // generate the header
 var head = labels.join("|");
-var line = labels.map(function() { return "-----" }).join("|");
+var line = labels.map(function() { return "-----"; }).join("|");
 
 // simple abstraction to generate a link
 function marklink(name, url, url_prefix) {
@@ -41,6 +41,6 @@ var body = meetups.map(function(meetup) {
     return p + "\n" + c;
 });
 
-console.log(head)
-console.log(line)
-console.log(body)
+console.log(head);
+console.log(line);
+console.log(body);
